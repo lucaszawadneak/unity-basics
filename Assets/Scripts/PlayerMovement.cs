496816fbc07617bf2fb04f4c91876fb8fe6f2182
Lucas Zawadneak <lucaszawadneak@gmail.com>
2022-09-27 11:39:01 -0300

    feat:player idle animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,25 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
 
-    private RigidBody2D body;
-    // Start is called before the first frame update
-    void Start()
+  private Rigidbody2D body;
+  [SerializeField] private float speed;
+
+  private void Awake()
+  {
+    body = GetComponent<Rigidbody2D>();
+  }
+
+  private void Update()
+  {
+    float horizontal = Input.GetAxis("Horizontal");
+    body.velocity = new Vector2(horizontal * speed, body.velocity.y);
+
+    if (horizontal > 0.01f)
     {
-        
+      transform.localScale = new Vector3(1, 1, 1);
+    }
+    else if (horizontal < -0.01f)
+    {
+      transform.localScale = new Vector3(-1, 1, 1);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    if (Input.GetKey(KeyCode.Space))
     {
-        
+      body.velocity = new Vector2(body.velocity.x, 5f);
     }
 
-    Awake()
-    {
-        body = GetComponent<RigidBody2D>();
-    }
+  }
 }
